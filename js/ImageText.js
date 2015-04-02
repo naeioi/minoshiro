25f1478dfe89ce9b99b82fd76dca1ac2ff8ef194
nae.ikari <zhukrmail@gmail.com>
2015-04-03 06:21:52 +0800

    add ImageText & TextLine class

---
+++ js/ImageText.js
@@ -0,0 +1,108 @@
+/**
+ * Created by naeioi on 2015/4/3.
+ */
+/*
+ ImageText
+ ImageText由Container继承而来。
+ ImageText以png形式保存文字。可能包含多行。
+
+ Property
+ originText  @type String
+ 原始字符串。以'\n'表示换行
+ reg     @type integer
+ 参考点。0左上1右上2右下3坐下
+ texts   @type array of TextLine
+ 文字数组。每个元素为一行字母
+ dir     @type integer
+ 文字方向。0横1竖
+ letterSpacing   @type integer
+ 文字间距。
+ fontSize    @type integer
+ 文字大小。
+ fontFamily  @type String
+ 字体。
+ color   @type string
+ 颜色。
+
+ Method
+ load(string)
+ 载入string表示的字符串
+
+ Event
+ click
+ 当text被单击时触发。事件原型同control_bar中click_handler
+*/
+
+define(['createjs', function(createjs) {
+    function ImageText(originText, reg, dir, space, size, family, color)
+    {
+        this.Container_constructor();
+
+        this.originText = originText;
+        this.reg = reg;
+        this.letterSpacing = space;
+        this.fontSize = size;
+        this.fontFamily = family;
+        this.color = color;
+
+        this.texts = [];
+
+        this.load(originText);
+    }
+
+    var p = createjs.extend(ImageText, createjs.Container);
+
+    p.imageLoder = new Function();
+
+    p.config = function(imageLoader)
+    {
+        p.imageLoder = imageLoader;
+    }
+
+    p.load = function(str)
+    {
+        this.removeAllChildren();
+        this.texts.length = 0;
+
+        var arr = str.split('\n');
+
+        for(var i = 0; i < arr.size(); i++)
+        {
+            var textline = new TextLine(arr[i], dir, letterSpacing, fontSize, fontFamily, color);
+            var bound = textLine.getBounds();
+
+            switch(this.reg)
+            {
+                case 0:
+                    textline.set({
+                        x: 0,
+                        y: i*bound.height
+                    })
+                    break;
+                case 1:
+                    textline.set({
+                        x: -bound.width,
+                        y: i*bound.height
+                    })
+                    break;
+                case 2:
+                    textline.set({
+                        x: -bound.width,
+                        y: i*bound.height
+                    })
+                    break;
+                case 3:
+                    textline.set({
+                        x: 0,
+                        y: i*bound.height
+                    })
+                    break;
+            }
+
+            this.texts.push(textLine);
+            this.addChild(textline);
+        }
+    }
+
+    return createjs.promote(ImageText, "Container");
+}])
--- js/TextLine.js
+++ js/TextLine.js
@@ -1,3 +1,51 @@
 /**
  * Created by naeioi on 2015/4/3.
  */
+/*
+ TextLine
+ TextLine由Bitmap继承而来
+ TextLine保存一行文字的信息
+
+ Property
+ Text    @type string
+ 文字。
+ image   @type Bitmap
+ 图片格式的文字
+ dir     @type integer
+ 文字方向。0横1竖
+ letterSpacing   @type integer
+ 文字间距。
+ fontSize    @type integer
+ 文字大小。
+ fontFamily  @type String
+ 字体。
+ color   @type string
+ 颜色。
+ */
+define(['createjs'], function(createjs) {
+    function TextLine(text, dir, space, size, family, color) {
+        this.Bitmap_constructor();
+
+        this.text = text || "";
+        this.dir = dir || 0;
+        this.image = null;
+        this.letterSpacing = space || 0;
+        this.fontSize = size || 12;
+        this.fontFamily = family || "";     //需要选择默认字体
+        this.color = color || '#000000';
+
+        this.cursor = "text";
+
+        this.load(text);
+    }
+
+    var p = createjs.extend(TextLine, createjs.Bitmap);
+
+    p.load = function(text)
+    {
+        this.text = text;
+        this.image = p.imageLoader(this);
+    }
+
+    return createjs.promote(TextLine, "Bitmap");
+});
