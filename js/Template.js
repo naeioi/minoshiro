5d1a3a7cead665b03c2aefe725846ada9be9f895
nae.ikari <zhukrmail@gmail.com>
2015-04-14 23:30:57 +0800

    migrate template load from Model to Template

--- js/Template.js
+++ js/Template.js
@@ -4,9 +4,112 @@
 requirejs(["createjs"], function(createjs){
     function Template(){
         this.DisplayObject_Constructor();
+        this.elements = [];
     }
 
     var p = createjs.extend(Template, createjs.DisplayObject);
+
+    function getImage(src) {
+        var def = $.Deferred();
+        var img = document.createElement('img');
+        img.src = src;
+        img.onload(function(){
+            def.resolve(img);
+        });
+        return def.promise();
+    }
+
+    p.load = function(res) {
+        var t = this;
+
+        t.res = res;
+
+        var baseUrl = 'mode/';
+        t.bg_manualable = res.bg_manualable;
+
+        //scale to proper size in demo mode
+        var scaleX = res.demo_width / res.origin_width,
+            scaleY = res.demo_height / res.origin_height;
+
+        t.set({
+            width: res.demo_width,
+            height: res.demo_height
+        })
+
+        var def = $.Deferred();
+        def.resolve();
+
+        //load and set bg img
+        //note that the src of default bg is src[0], with others choices to be manually selected
+        if (res.bg && res.bg.length > 0) {
+            def = getImage(baseUrl + res.bg[0].src);
+            def = def.then(function (img) {
+                //img is <img> object, due to the inconvenience of base64
+                t.bg[0].img = img;
+                t.bg[0].x = (res.bg[0].x || 0) * scaleX;
+                t.bg[0].y = (res.bg[0].y || 0) * scaleY;
+                t.bg[0].scaleX = (res.bg[0].scaleX || 0) * scaleX;
+                t.bg[0].scaleY = (res.bg[0].scaleY || 0) * scaleY;
+            })
+        }
+
+        //load and set elements
+        def = def.then(function () {
+
+            //load the elements one by one(instead of in parallel)
+            var chain = $.Deferred();
+            chain.resolve();
+
+            for (var i = 0; i < t.elements.length; i++) {
+                (function () {
+                    var e = res.elements[i];
+
+                    chain = chain.then(function () {
+                        return getImage(baseUrl + e.src).then(function (img) {
+
+                            var obj = e;
+                            e.img = img;
+                            e.scaleX = scaleX;
+                            e.scaleY = scaleY;
+                            e.x = e.x * scaleX;
+                            e.y = e.y * scaleY;
+
+                            t.elements.push(obj);
+                        });
+                    })
+                })();
+            }
+
+            return chain.promise();
+        })
+
+        //load texts
+        def = def.then(function () {
+            var texts = res.texts;
+
+            var chain = $.Deferred();
+            chain.resolve();
+
+            for (var i = 0; i < texts.length; i++) {
+                (function () {
+                    var text = texts[i];
+                    var imgtext = new createjs.ImageText(text.context, text.reg, text.dir,
+                        text.space, Math.floor(text.size * scaleX),
+                        text.font, text.color);
+
+                    imgtext.x = imgtext.x / scaleX;
+                    imgtext.y = imgtext.y / scaleY;
+
+                    chain = chain.then(imgtext.load);
+                })();
+            }
+
+            chain.done(def.resolve);
+        })
+    }
+
+
+
     createjs.Template = Template;
     return createjs.prompt(Template, "DisplayObject");
 })
--- js/model.js
+++ js/model.js
@@ -8,94 +8,28 @@ define(['createjs', 'jquery', 'ImageText', 'Template'], function(createjs, jquer
 
     var p = create.extend(Model, createjs.Container);
 
-    Model.load = function(src, mode)
+
+
+    Model.load = function(src)
     {
-        mode = mode || "demo";
+        mode = "demo";
         var self = this;
         var t = new createjs.Template();
+        this.template = t;
 
         var def = $.get(src);
 
         def = def.then(function(res){
 
-            var baseUrl = mode+'/';
+            return t.load(res);
 
-            if(mode === 'demo'){
 
-                //scale to proper size in demo mode
-                var scaleX = res.demo_width  / res.origin_width,
-                    scaleY = res.demo_height / res.origin_height;
+        })
 
-                t.set({
-                    width: res.demo_width,
-                    height: res.demo_height
-                })
 
-                //load and set bg img
-                //note that the src of default bg is src[0], with others choices to be manually selected
-                var def = getImage(baseUrl + res.bg.src[0]);
-                def = def.then(function(img){
-                    //img is <img> object, due to the inconvenience of base64
-                    t.bg[0].img = img;
-                    t.bg[0].x = t.bg[0].y = 0;
-                    t.bg[0].scaleX = t.width / img.width;
-                    t.bg[0].scaleY = t.height / img.height;
-                })
+        //add all staff into container
+        def = def.then(function(){
 
-                //load and set elements
-                def = def.then(function(){
-
-                    //load the elements one by one(instead of in parallel)
-                    var chain = $.Deferred();
-                    chain.resolve();
-
-                    for(var i = 0; i < t.elements.length; i++){
-                        var e = res.elements[i];
-
-                        chain = chain.then(function(){
-                            return getImage(baseUrl + e.src).then(function(img){
-
-                                var obj = e;
-                                e.img = img;
-                                e.scaleX = scaleX;
-                                e.scaleY = scaleY;
-                                e.x = e.x * scaleX;
-                                e.y = e.y * scaleY;
-
-                                t.elements.push(obj);
-                            });
-                        })
-                    }
-
-                    return chain.promise();
-                })
-
-                //load texts
-                def = def.then(function(){
-
-                    var texts = res.texts;
-
-                    var chain = $.Deferred();
-                    chain.resolve();
-
-                    for(var i = 0; i < texts.length; i++){
-                        var text = texts[i];
-                        var imgtext = new createjs.ImageText(text.context, text.reg, text.dir,
-                                                             text.space, Math.floor(text.size * scaleX),
-                                                             text.font, text.color);
-
-                        imgtext.x = imgtext.x / scaleX;
-                        imgtext.y = imgtext.y / scaleY;
-
-                        chain = chain.then(imgtext.load);
-                    }
-
-                    chain.done(def.resolve);
-                })
-            }
-            else if(mode === 'origin'){
-
-            }
         })
 
         return def.promise();
